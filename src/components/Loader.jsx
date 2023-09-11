90c2d9b7f3ea709515feb4573d009c62b5198fad
othea32 <othea32@gmail.com>
2023-09-10 20:20:35 -0700

    modified Welcome and Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,10 @@
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex justify-center items-center py-3">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700" />
+      Loader
+    </div>
   )
 }
 
